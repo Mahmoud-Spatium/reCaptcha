5958ac1ddfc801803988ccad6ff9f4abaddbf1b9
Mahmoud-Spatium <mahmoud-mohammed@spatiumsoftware.com>
2024-06-03 10:17:58 +0300

    reCaptcha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { RecaptchaModule } from 'ng-recaptcha';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecaptchaModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reCaptcha';
+  // captcha response
+  resolved(captchaResponse: any) {
+    console.log(captchaResponse);
+  }
 }
